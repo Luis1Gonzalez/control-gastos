45976e3a3968237feb0648f8a497b2181d1e1f66
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-10-30 21:59:46 +0100

    primer commit

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -5,9 +5,9 @@ const ControlPresupuesto = () => {
     <div className='bg-white text-blue-400 w-8/12 rounded-md my-3 py-5 flex justify-center'>
 
 <form className='w-full flex  justify-center'>
-    <div className=' flex flex-col w-8/12 text-xs items-center'>
+    <div className=' flex flex-col w-[90%] text-lg items-center box-border'>
         <label htmlFor="" className='text-center'>Definir Presupuesto</label>
-        <input type="text" className=' rounded-md bg-gray-100 my-2 sm:w-full'/>
+        <input type="text" className=' rounded-md bg-gray-100 my-2 w-[100%] sm:w-full'/>
         <input type="submit" value="Añadir" className='rounded bg-blue-400 text-white w-24 py-0.5 cursor-pointer'/>
     </div>
 </form>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import ControlPresupuesto from './ControlPresupuesto'
 
 const Header = () => {
   return (
-    <header className='bg-blue-400 flex flex-col p-1 items-center rounded-md text-white w-[90%] sm:w-[60%] lg:w-[40%]'>
-        <h1>Control de Gastos</h1>
+    <header className='bg-blue-400 flex flex-col p-3 items-center rounded-md text-white w-[90%] sm:w-[60%] lg:w-[40%]'>
+        <p className='text-xl mt-2'>Control de Gastos</p>
 
         <ControlPresupuesto />
     </header>
