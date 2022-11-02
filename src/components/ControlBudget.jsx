20a747639158c581471b04534fe632e666aec54f
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-11-02 21:01:10 +0100

    agregando el circlePreogresBar

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -24,16 +24,17 @@ const ControlBudget = ({ budget, expenses }) => {
 
         setTimeout(() => {
             setPercentage(newPercentage)
+            if(newPercentage > 200 ){
+                setMessage('Posible Error Entre el Presupuesto y sus Gastos')
+                setAvailable(0)
+            }
         },1500)
 
-        if(percentage > 200 ){
-            setMessage('Posible Error Entre el Presupuesto y sus Gastos')
-            setAvailable(0)
-        }
+        
     }, [expenses])
 
     return (
-        <div className='bg-white text-blue-500 w-[90%] min-h-[220px] md:w-[75%] font-bold rounded-md my-4 px-2 flex flex-col items-center md:flex-row'>
+        <div className='bg-white text-blue-500 w-[90%] min-h-[220px] md:w-[75%] font-bold rounded-md my-4 px-2 flex flex-col items-center sm:flex-row'>
             <div className='w-[70%] md:w-2/4 flex justify-center pt-3 h-[160px]'>
                 <CircularProgressbar 
                     styles={buildStyles({
@@ -45,7 +46,7 @@ const ControlBudget = ({ budget, expenses }) => {
                     text={percentage > 200 ? '!Error' : `${percentage}%`}
                 />
             </div>
-            <div className='my-1 md:w-2/4 flex flex-col justify-around h-full pt-6'>
+            <div className='my-1 md:w-2/4 flex flex-col justify-around h-full md:py-12'>
                 <p>Presupuesto:<span className='text-gray-500 font-normal'>{` ${formatCurrency(budget)}`}</span></p>
                 <p>Disponible:<span className='text-gray-500 font-normal'>{` ${formatCurrency(available)}`}</span></p>
                 <p>Gastado:<span className='text-gray-500 font-normal'>{` ${formatCurrency(spent)}`}</span></p>
