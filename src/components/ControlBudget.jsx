922b2554a55d13ab630a9542d004f14e6a156474
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-11-02 20:44:30 +0100

    agregando el circlePreogresBar

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { formatCurrency } from './helpers'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import "react-circular-progressbar/dist/styles.css"
+import Message from './Message'
+import { Children } from 'react'
 
 const ControlBudget = ({ budget, expenses }) => {
 
     const [spent, setSpent] = useState(0);
     const [available, setAvailable] = useState(0);
     const [percentage, setPercentage] = useState(0)
+    const [message, setMessage] = useState('')
 
     useEffect(() => {
         const totalSpent = expenses.reduce((total, expense) => expense.quantity + total, 0)
@@ -16,35 +19,39 @@ const ControlBudget = ({ budget, expenses }) => {
 
         const newPercentage = (((budget - totalAvailable) / budget) * 100).toFixed(1)
 
-        console.log(totalSpent)
-        console.log(totalAvailable)
-
         setSpent(totalSpent)
         setAvailable(totalAvailable)
 
         setTimeout(() => {
             setPercentage(newPercentage)
         },1500)
+
+        if(percentage > 200 ){
+            setMessage('Posible Error Entre el Presupuesto y sus Gastos')
+            setAvailable(0)
+        }
     }, [expenses])
 
     return (
-        <div className='bg-white text-blue-500 w-[90%] h-[220px] md:w-[50%] font-bold rounded-md my-4 px-2 flex flex-col items-center'>
-            <div className='w-[70%] flex justify-center pt-3 h-[120px]'>
+        <div className='bg-white text-blue-500 w-[90%] min-h-[220px] md:w-[75%] font-bold rounded-md my-4 px-2 flex flex-col items-center md:flex-row'>
+            <div className='w-[70%] md:w-2/4 flex justify-center pt-3 h-[160px]'>
                 <CircularProgressbar 
                     styles={buildStyles({
                         pathColor: percentage > 100 ? '#dc2626' : '#3b82f6',
                         trailColor: '#f5f5f5',
-                        textColor: percentage > 100 ? '#dc2626' : '#3b82f6'
+                        extColor: percentage < 100 ? '#dc2626' : '#3b82f6'
                     })}
                     value={percentage}
-                    text={`${percentage}%`}
+                    text={percentage > 200 ? '!Error' : `${percentage}%`}
                 />
             </div>
-            <div className='my-3'>
+            <div className='my-1 md:w-2/4 flex flex-col justify-around h-full pt-6'>
                 <p>Presupuesto:<span className='text-gray-500 font-normal'>{` ${formatCurrency(budget)}`}</span></p>
                 <p>Disponible:<span className='text-gray-500 font-normal'>{` ${formatCurrency(available)}`}</span></p>
                 <p>Gastado:<span className='text-gray-500 font-normal'>{` ${formatCurrency(spent)}`}</span></p>
+                {message && <Message type="error">{message}</Message>}
             </div>
+            
         </div>
     )
 }
--- src/components/NewBudget.jsx
+++ src/components/NewBudget.jsx
@@ -25,14 +25,14 @@ return (
 
 
 
-  <div className='bg-white text-blue-400 w-full rounded-md my-3 py-5 flex justify-center'>
+  <div className='bg-white text-blue-400 w-full h-[220px] md:w-[75%] rounded-md my-3 py-5 flex justify-center'>
 
     <form className='w-full flex flex-col  items-center' onSubmit={handleBudget}>
       <div className=' flex flex-col w-[90%] text-xl items-center box-border'>
-        <label htmlFor="budget" className='text-center'>Definir Presupuesto</label>
+        <label htmlFor="budget" className='text-center my-3'>Definir Presupuesto</label>
         <input type="text" id="budget" value={budget} placeholder='Añade tu presupuesto' className=' rounded-md bg-gray-100 my-2 w-[100%] p-1 sm:w-full text-center placeholder:text-gray-300 placeholder:text-md placeholder:italic' onChange={(e) => setBudget(Number(e.target.value))} />
       </div>
-      <input type="submit" value="Añadir" className='shadow-box rounded bg-blue-800 text-white w-36 py-0.5 cursor-pointer text-center' />
+      <input type="submit" value="Añadir" className='shadow-box rounded my-2 bg-blue-800 text-white w-36 py-0.5 cursor-pointer text-center' />
       {message && <Message type="error">{message}</Message>}
     </form>
 
