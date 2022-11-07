304d7381236757e27b05b18a261511768dc03678
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-11-07 22:13:03 +0100

    eliminado el swipeableList

--- src/components/UnitExpense.jsx
+++ src/components/UnitExpense.jsx
@@ -8,8 +8,7 @@ import IconoSalud from './../assets/img/icono_salud.svg'
 import IconoSuscripciones from './../assets/img/icono_suscripciones.svg'
 import { formatDate } from './helpers'
 import { formatCurrency } from './helpers'
-import { FiEdit3 } from 'react-icons/fi';
-import { RiDeleteBack2Line } from 'react-icons/ri';
+import { TiDeleteOutline } from 'react-icons/ti';
 
 
 const categoryDictionary = {
@@ -33,7 +32,7 @@ const UnitExpense = ({ expense, deleteExpense, setExpenseEdit }) => {
 
                 <div className='flex flex-col py-3 px-1 sm:p-3 w-full sm:flex-row shadow-box text-gray-700 my-3'>
 
-                    <div className='cursor-pointer h-auto flex py-1 items-center sm:w-3/5 md:mx-3' onClick={() => setExpenseEdit(expense)}>
+                    <div className='cursor-pointer h-auto flex py-1 items-center sm:w-3/4 md:mx-3' onClick={() => setExpenseEdit(expense)}>
                         <img className='h-[65px] mx-1 md:mx-3' src={categoryDictionary[category]} alt="iconos de los gastos" />
                         <div className=''>
                             <p className='uppercase text-sm sm:text-2xl font-bold'>{category}</p>
@@ -41,9 +40,9 @@ const UnitExpense = ({ expense, deleteExpense, setExpenseEdit }) => {
                             <p className='text-xs sm:text-xl font-bold'>Agregado el:{''} <span className='font-normal capitalize text-xs sm:text-lg md:text-sm lg:text-lg'>{formatDate(dateNow)}</span></p>
                         </div>
                     </div>
-                    <div className='flex mt-2 px-2 items-center'>
-                    <button onClick={() => deleteExpense(id)} className=' shadow-box bg-red-600 sm:w-[40px] sm:h-[40px] text-white rounded-full md:mx-3 flex justify-center h-[30px] w-[30px] items-center sm:px-3'>{<RiDeleteBack2Line />}</button>
-                    <p className='flex w-5/6 justify-end items-center px-5 sm:mx-8 font-bold md:w-2/4 text-md sm:text-xl lg:text-3xl sm:w-1/4 sm:-order-1'>{formatCurrency(quantity)}</p>
+                    <div className='flex mt-2 pl-5 items-center sm:w-1/4'>
+                    <button onClick={() => deleteExpense(id)} className='sm:w-[px] sm:h-1/4 md:mx-1 flex justify-center h-[30px] w-[30px] text-2xl text-red-600 items-center'>{<TiDeleteOutline />}</button>
+                    <p className='flex w-5/6 justify-end items-center px-2 font-bold sm:w-auto text-md sm:text-xl lg:text-3xl -order-1'>{formatCurrency(quantity)}</p>
                     </div>
                 </div>
 
